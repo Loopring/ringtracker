5bb23e73a4f1f25748e63f41b817b5b885d35692
lyd <lyddream@126.com>
2018-08-22 15:01:42 +0800

    api

--- src/common/utils/relay.js
+++ src/common/utils/relay.js
@@ -13,3 +13,65 @@ export async function getTokens() {
 
 
 // -------------------------- li start ---------------------------
+
+export function getAllRelayers(params) {
+  let body = {};
+  body.method = 'loopring_getAllRelayers';
+  body.params = [{...params}];
+  return request({
+    method: 'post',
+    body
+  })
+}
+
+export function getRelayerDetail(params) {
+  let body = {};
+  body.method = 'loopring_getRelayerDetail';
+  body.params = [{...params}];
+  return request({
+    method: 'post',
+    body
+  })
+}
+
+
+export function getAllDexs(params) {
+  let body = {};
+  body.method = 'loopring_getAllDexs';
+  body.params = [{...params}];
+  return request({
+    method: 'post',
+    body
+  })
+}
+
+
+export function getDexDetail(params) {
+  let body = {};
+  body.method = 'loopring_getDexDetail';
+  body.params = [{...params}];
+  return request({
+    method: 'post',
+    body
+  })
+}
+
+export function getAllRings(params) {
+  let body = {};
+  body.method = 'loopring_getAllRings';
+  body.params = [{...params}];
+  return request({
+    method: 'post',
+    body
+  })
+}
+
+export function getRingDetail(params) {
+  let body = {};
+  body.method = 'loopring_getRingDetail';
+  body.params = [{...params}];
+  return request({
+    method: 'post',
+    body
+  })
+}
