17e2e0f222cd54759c672b9eafd86eb7afc6afc3
lyd <lyddream@126.com>
2018-08-29 18:44:37 +0800

    complete ring

--- src/ringtracker/pages/Rings/RingDetail.js
+++ src/ringtracker/pages/Rings/RingDetail.js
@@ -2,6 +2,13 @@ import React from 'react'
 import config from '../../../common/config'
 import {Spin, Card} from 'antd'
 import Fills from '../Fills'
+import schema from '../../../modules/rings/schema';
+import intl from 'react-intl-universal'
+import {toBig} from "LoopringJS/common/formatter"
+import {getFormatNum} from "../../../modules/formatter/common";
+import TokenFormatter from '../../../modules/tokens/TokenFm'
+
+
 
 export default class RingDetail extends React.Component {
 
@@ -21,20 +28,56 @@ export default class RingDetail extends React.Component {
           this.setState({loading: false})
         }
       })
-
   }
-
-
   render() {
     const {item, loading} = this.state;
+    const fills = item.fills && item.fills.map(fill => {
+      const tokenS = config.getTokenByAddress(fill.tokenS).symbol
+      const tokenB = config.getTokenByAddress(fill.tokenB).symbol
+      return {...fill,tokenS,tokenB}
+    });
+    const getSplitFee = (splitFee) => {
+      let totalSplitFee = '';
+      if (splitFee) {
+        for (let key in splitFee) {
+          const tf = new TokenFormatter({symbol:key})
+          const token = config.getTokenBySymbol(key);
+          totalSplitFee = totalSplitFee !== '' ? totalSplitFee + " + " : totalSplitFee;
+
+          totalSplitFee = totalSplitFee + tf.toPricisionFixed(tf.getUnitAmount(toBig(splitFee[key]))).concat(' ' + key)
+        }
+      }
+      return totalSplitFee
+    };
+
+    const renders = {
+      ringHash:(value,item,index)=><div className="text-truncate d-block" >{value}</div>,
+      delegateAddress:(value,item,index)=> <a className="text-truncate d-block" target="_blank" href={`https://etherscan.io/address/${value}`}>{value}</a>,
+      miner:(value,item,index)=> <a className="text-truncate d-block" style={{}} target="_blank" href={`https://etherscan.io/address/${value}`}>{value}</a>,
+      txHash:(value,item,index)=> <a className="text-truncate d-block" style={{}} target="_blank" href={`https://etherscan.io/tx/${value}`}>{value}</a>,
+      blockNumber:(value,item,index)=> <a className="text-truncate d-block" style={{}} target="_blank" href={`https://etherscan.io/block/${value}`}>{value}</a>,
+      protocol:(value,item,index)=> <a className="text-truncate d-block" style={{}} target="_blank" href={`https://etherscan.io/address/${value}`}>{value}</a>,
+      totalSplitFee:(value,item,index) => getSplitFee(value)
+    }
 
     return (
       <div>
-        <Card title="Ring Information">
-          TODO Chart
+        <Card title={intl.get('ring_detail.ring_detail')}>
+          {
+            item.ringInfo && schema.map((field,index)=>
+              <div className="row pb10" key={index}>
+                <div className="col-2 color-grey-700">{field.title()}</div>
+                <div className="col color-grey-700 text-left">
+                  {renders[field.name] &&  renders[field.name](item.ringInfo[field.name],item.ringInfo)}
+                  {!renders[field.name]&&  field.formatter && field.formatter(item.ringInfo[field.name])}
+                  {!renders[field.name]&&  !field.formatter  && item.ringInfo[field.name]}
+                </div>
+              </div>
+            )
+          }
         </Card>
-        <Card title="Trades of Ring">
-          <Fills.FillTable fills = {{items:item.fills,loading:loading}}/>
+        <Card title={intl.get('ring_detail.fill_table')}>
+          <Fills.FillTable fills = {{items:fills,loading:loading}}/>
         </Card>
       </div>
     )
