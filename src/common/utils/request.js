cb472d62c0d5f84388e86edb207e009b4eb1f742
dyfvicture <fbi-ceo@163.com>
2018-08-31 16:57:46 +0800

    request url

--- src/common/utils/request.js
+++ src/common/utils/request.js
@@ -12,7 +12,7 @@ function parseJSON(res) {
 }
 
 let checkHost = () => {
-  const relayHost = "//10.137.105.199:8083"
+  const relayHost = "//13.112.62.24"
   window.LOOPRING_PROVIDER_HOST = relayHost + '/rpc/v2'
   window.ETH_HOST = relayHost + '/eth'
 
