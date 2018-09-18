3889656d5979a21a71be1dc0f086dee324d85dfc
dyfvicture <fbi-ceo@163.com>
2018-09-18 11:13:15 +0800

    search

--- src/ringtracker/pages/Search/FillTable.jsx
+++ src/ringtracker/pages/Search/FillTable.jsx
@@ -16,44 +16,95 @@ export default function ListMyFills(props) {
           <div>
             <table className="table table-responsive fs14" >
               <thead className="border-none">
-                  <tr className="border-none">
-                      <th className="border-none">{intl.get('ring.ringIndex')}</th>
-                      <th className="border-none">{intl.get('common.market')}</th>
-                      <th className="border-none">{intl.get('common.side')}</th>
-                      <th className="border-none">{intl.get('common.amount')}</th>
-                      <th className="border-none">{intl.get('common.price')}</th>
-                      <th className="border-none">{intl.get('common.total')}</th>
-                      <th className="border-none">{intl.get('title.lrc_fee')}</th>
-                      <th className="border-none">{intl.get('title.created')}</th>
-                      <th className="border-none">{intl.get('title.options')}</th>
-                  </tr>
+              <tr className="border-none">
+                <th className="border-none">{intl.get('ring.txHash')}</th>
+                <th className="border-none">{intl.get('common.market')}</th>
+                <th className="border-none">{intl.get('common.side')}</th>
+                <th className="border-none">{intl.get('common.buy')}</th>
+                <th className="border-none">{intl.get('common.sell')}</th>
+                <th className="border-none">{intl.get('common.price')}</th>
+                <th className="border-none">{intl.get('title.lrc_fee')}</th>
+                <th className="border-none">{intl.get('ring.miner')}</th>
+                <th className="border-none">{intl.get('trade.relay')}</th>
+                <th className="border-none">{intl.get('ring.title')}</th>
+                <th className="border-none">{intl.get('common.time')}</th>
+                {
+                  false &&
+                  <th className="border-none">{intl.get('title.options')}</th>
+                }
+              </tr>
               </thead>
               <tbody className="">
-                {
-                  fills && fills.items && fills.items.map((item,index)=>{
-                    const fillFm = new FillFm(item)
-                    const actions = {
-                      goToRingDetail:()=>routeActions.gotoPath(`/rings/${item.ringIndex}`)
+              {
+                fills.items && fills.items.map((item,index)=>{
+                  const fillFm = new FillFm(item)
+                  const actions = {
+                    goToRingDetail:(e)=>{
+                      e.stopPropagation()
+                      e.preventDefault()
+                      routeActions.gotoPath(`/rings/${item.ringIndex}?delegateAddress=${item.delegateAddress}`)
+                    },
+                    goToTradeDetail:(e)=>{
+                      routeActions.gotoPath(`/trades/${item.ringIndex}/${item.fillIndex}?d=${item.delegateAddress}`)
+                    },
+                    goToEtherscan:(url, e) => {
+                      e.stopPropagation()
+                      e.preventDefault()
+                      window.open(url)
+                    },
+                    goToRelay:(relayName, e)=>{
+                      e.stopPropagation()
+                      e.preventDefault()
+                      if(relayName) {
+                        routeActions.gotoPath(`/relays/${relayName}`)
+                      }
                     }
-                    return (
-                      <tr key={index}>
-                        <td>{renders.ringIndex(fillFm,actions)}</td>
-                        <td>{item.market}</td>
-                        <td>{renders.side(fillFm)}</td>
-                        <td>{fillFm.getAmount()}</td>
-                        <td>{fillFm.getPrice()}</td>
-                        <td>{fillFm.getTotal()}</td>
-                        <td>{fillFm.getLRCFee()}</td>
-                        <td>{fillFm.getCreateTime()}</td>
-                        <td><a onClick={routeActions.gotoPath.bind(this,`/trades/${item.ringIndex}/${item.fillIndex}?d=${item.delegateAddress}`)}>{intl.get('options.view_detail')}</a></td>
-                     </tr>
-                    )
-                  })
-                }
-                {
-                  fills && fills.items && fills.items.length === 0 &&
-                  <tr><td colSpan='100'><div className="text-center">{intl.get('common.list.no_data')}</div></td></tr>
-                }
+                  }
+                  const tokens = item.market.split('-')
+                  return (
+                    <tr key={index} onClick={actions.goToTradeDetail} className="cursor-pointer">
+                      <td className="text-nowrap">
+                        <a className="fs13" onClick={actions.goToEtherscan.bind(this, `https://etherscan.io/tx/${item.txHash}`)}>
+                          {fillFm.getShortTxHash()}
+                        </a>
+                      </td>
+                      <td className="text-nowrap">{item.market}</td>
+                      <td className="text-nowrap">{renders.side(fillFm)}</td>
+                      <td className="text-nowrap">{fillFm.getBuy()}</td>
+                      <td className="text-nowrap">{fillFm.getSell()}</td>
+                      <td className="text-nowrap">{`${fillFm.getPrice()} ${tokens[1]}`}</td>
+                      <td className="text-nowrap">{fillFm.getLRCFee()}</td>
+                      <td className="text-nowrap">
+                        <a className="fs13" onClick={actions.goToEtherscan.bind(this, `https://etherscan.io/address/${item.Miner}`)}>
+                          {fillFm.getShortMiner()}
+                        </a>
+                      </td>
+                      <td className="text-nowrap">
+                        <a className="fs13" onClick={actions.goToRelay.bind(this, item.relay)}>
+                          {item.relay || 'Loopring'}
+                        </a>
+                      </td>
+                      <td className="text-nowrap">
+                        <a className="fs13" onClick={actions.goToRingDetail}>
+                          {fillFm.getRingIndex()}
+                        </a>
+                      </td>
+                      <td className="text-nowrap">{fillFm.getCreateTime()}</td>
+                      {
+                        false &&
+                        <td className="text-nowrap">
+                          <a className="fs13 rounded bg-primary-3 text-primary pl10 pr10 pt5 pb5 text-center" onClick={routeActions.gotoPath.bind(this,`/trades/${item.ringIndex}/${item.fillIndex}?d=${item.delegateAddress}`)}>{intl.get('options.view_detail')}</a>
+                        </td>
+                      }
+
+                    </tr>
+                  )
+                })
+              }
+              {
+                fills.items && fills.items.length === 0 &&
+                <tr><td colSpan='100'><div className="text-center">{intl.get('common.list.no_data')}</div></td></tr>
+              }
               </tbody>
             </table>
           </div>
@@ -73,10 +124,10 @@ export default function ListMyFills(props) {
     },
     side: (fm) => {
       if (fm.fill.side === 'sell') {
-        return <div className="text-error">{intl.get('common.sell')}</div>
+        return <div className="color-white bg-error rounded text-center pl10 pr10 pt5 pb5">{intl.get('common.sell')}</div>
       }
       if (fm.fill.side === 'buy') {
-        return <div className="text-success">{intl.get('common.buy')}</div>
+        return <div className="color-white bg-success rounded text-center pl10 pr10 pt5 pb5">{intl.get('common.buy')}</div>
       }
     },
   }
